2aacbe24973ed3c6ae1c5adf1584b5f5a7d0ff06
manish <manishkumar60708090@gmail.com>
2022-09-03 13:44:04 +0530

    complete signin page

---
+++ pages/auth/signin.js
@@ -0,0 +1,46 @@
+
+import React from "react";
+import {getProviders , signIn} from "next-auth/react"
+import { async } from "@firebase/util";
+import Header from "../../components/Header"
+
+
+
+export default function signin({providers}){
+    return(
+        <>
+        <Header>
+             
+        </Header>
+        <div className="flex justify-center space-x-7 mt-20">
+            <img className="hidden object-cover rotate-6 md:inline-flex "  src="https://jetdigital.in/wp-content/uploads/2021/08/instagram-6560837_640.png" alt="" />
+            <div className="">
+                {
+                    Object.values(providers).map((provider)=>(
+                        <div className="flex flex-col items-center" key={provider.name}>
+                            <img  className="w-32 object-cover " src="https://support.pingidentity.com/servlet/servlet.FileDownload?file=00P1W00001JyxeKUAR" alt="" />
+                            <p className="text-sm italic my-10 text-center">This app is created for learning purpases</p>
+                            <button className="bg-red-400 rounded-lg p-3 text-white hover:bg-blue-600" onClick={()=>signIn(provider.id , {callbackUrl:"/"})}>Sign in with {provider.name}</button>
+                        </div>
+                    ))
+                }
+
+            </div>
+        </div>
+        
+        </>
+        
+    )
+}
+
+export async function getServerSideProps(context){
+    const providers = await getProviders();
+    return {
+        props:{providers}
+    }
+    return(
+        <div className="">
+            
+        </div>
+    )
+}
